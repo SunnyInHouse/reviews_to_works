544deb4c7c2e367de93fe4a930b9e1450762a07a
baby-platom <platon200502@gmail.com>
2021-08-16 17:59:48 +0600

    текст коммита

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,12 +1,12 @@
 from rest_framework import permissions
 
 
-class IsAuthorOrReadOnlyPermission(permissions.BasePermission):
+class IsAdminOrReadOnlyPermission(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
 
         return (
-            request.user.role == 'admin'
+            request.user.role == 'admin' or request.user.is_superuser
         )
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -5,27 +5,28 @@ from django_filters.rest_framework import DjangoFilterBackend
 from reviews.models import Genre, Category, Title
 from .serializers import (GenreSerializer, CategorySerializer,
                           TitleSerializer, TitleSerializerList)
+from .permissions import IsAdminOrReadOnlyPermission
 
 
 class GenreViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
                    mixins.DestroyModelMixin, viewsets.GenericViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAdminOrReadOnlyPermission,)
 
 
 class CategoryViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
                       mixins.DestroyModelMixin, viewsets.GenericViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAdminOrReadOnlyPermission,)
 
 
 class TitleViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
                    mixins.DestroyModelMixin, viewsets.GenericViewSet):
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAdminOrReadOnlyPermission,)
     filter_backends = (DjangoFilterBackend, filters.SearchFilter)
     filterset_fields = ('category__slug', 'genre__slug')
     search_fields = ('name', 'year')
