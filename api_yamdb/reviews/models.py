974f1fb934ea141b88ca5efcad19bd49c485e626
Ruslan Rakhmatullin <Ruslan964@yandex.ru>
2021-08-16 11:15:10 +0300

    Добавил все материалы, но осталась та же ошибка (not "application/json)

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+# временно создаю этот класс, чтобы можно было тестировать
+class Titles(models.Model):
+    name = models.TextField(verbose_name="Содержание отзыва")
+    year = models.IntegerField(verbose_name="Год")
+    category_id = models.IntegerField(verbose_name="Категория")
+
+
+class Reviews(models.Model):
+    title = models.ForeignKey(
+        Titles,
+        on_delete=models.CASCADE,
+        verbose_name="Отзывы",
+        related_name="reviews",
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="Автор отзыва",
+        related_name="reviews",
+    )
+    score = models.IntegerField(verbose_name="Оценка")
+    text = models.TextField(verbose_name="Содержание отзыва")
+    pub_date = models.DateTimeField(
+        auto_now_add=True, verbose_name="Дата отзыва"
+    )
+
+
+class Comments(models.Model):
+    review = models.ForeignKey(
+        Reviews,
+        on_delete=models.CASCADE,
+        verbose_name="Комментарии к отзыву",
+        related_name="comment",
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="Автор комментария",
+        related_name="comment",
+    )
+    text = models.TextField(verbose_name="Содержание отзыва")
+    pub_date = models.DateTimeField(
+        auto_now_add=True, verbose_name="Дата комментария"
+    )
